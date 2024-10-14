1686c4088d363b9132e88fc70a93da02c295bf6a
littlebean47 <jalynb48@gmail.com>
2024-10-14 15:38:15 -0400

    Login will display if there is no username

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,24 @@
+"use client"
 import ChatContainer from "./components/ChatContainer/ChatContainer";
 import Sidebar from "./components/Sidebar/Sidebar";
 import styles from "./page.module.css";
+import Login from "./pages/login/page";
 
 export default function Home() {
+  const username = localStorage.setItem("username", "jello")
+  if (typeof window !== 'undefined') {
+    console.log('Currently on Client side')
+  } else {
+    console.log('Currently on Server Side');
+  }
   return (
-    <div className={styles.app}>
-      <Sidebar />
-      <ChatContainer />
+    <div >
+      {!username ? <Login /> :
+        <div className={styles.app}>
+          <Sidebar />
+          <ChatContainer />
+        </div>
+      }
     </div>
   );
 }
