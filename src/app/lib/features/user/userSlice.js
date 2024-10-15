3067161669bb1a4e8451cdadfa6e93faa2939fb9
littlebean47 <jalynb48@gmail.com>
2024-10-15 12:24:11 -0400

    Added redux files to project

---
+++ src/app/lib/features/user/userSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        value: "",
+    },
+    reducers: {
+        login: (state, action) => {
+            // Redux Toolkit allows us to write "mutating" logic in reducers. It
+            // doesn't actually mutate the state because it uses the Immer library,
+            // which detects changes to a "draft state" and produces a brand new
+            // immutable state based off those changes.
+            // Also, no return statement is required from these functions.
+            state.value += action.payload
+        },
+        logout: (state) => {
+            state.value = ""
+        },
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { login, logout } = userSlice.actions
+
+export default userSlice.reducer
